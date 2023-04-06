0ec8d86c6e78257c7924cc6569b6eb01e1afe77d
hungthinhluu <hungthinhluu@Friday.Friday.com>
2023-04-06 14:15:55 +0700

    add IoT csv

--- kafka2EventHub.py
+++ kafka2EventHub.py
@@ -1,10 +1,11 @@
 import pandas as pd
 import json
-df = pd.read_csv('VNI-test.csv')
+# df = pd.read_csv('VNI-test.csv')
+df = pd.read_csv('IOT-temp.csv')
 
-list_price = ["Price", "Open", "High", "Low"]
-for i in list_price:
-    df[i] = pd.to_numeric(df[i].apply(lambda x: x.replace(",", "")))
+# list_price = ["Price", "Open", "High", "Low"]
+# for i in list_price:
+#     df[i] = pd.to_numeric(df[i].apply(lambda x: x.replace(",", "")))
 
 BOOTSTRAP_SERVER = 'friday.servicebus.windows.net:9093'
 TOPIC = 'stock'
@@ -17,7 +18,7 @@ producer = KafkaProducer(bootstrap_servers=[BOOTSTRAP_SERVER],
                         sasl_plain_password = 'Endpoint=sb://friday.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=cX20chc/DHcQwSatk+oInoN6+7z20KnyI+AEhDB0034=')
 
 col = []
-for i in range(0, 7):
+for i in range(0, 5):
     col.append(df.columns[i])
 
 def send(tmp):
@@ -25,7 +26,7 @@ def send(tmp):
     for i in range(0,6):
         s = s + col[i] + ": " + str(tmp[i]) + "-"'''
     s = {}
-    for i in range(0, 7):
+    for i in range(0, 5):
         s[col[i]] = tmp[i]
     print("Message: ", s)
     producer.send(TOPIC, value = json.dumps(s).encode('utf-8'))
